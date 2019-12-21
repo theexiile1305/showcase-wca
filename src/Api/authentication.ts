5a6bc2fa34d3cb25f3073deeccf083d31aa20cc5
theexiile1305 <michael.fuchs@hm.edu>
2019-12-21 16:32:23 +0100

    basic authentication with signIn and signOut

---
+++ src/Api/authentication.ts
@@ -0,0 +1,38 @@
+
+import {
+  setUILoading, clearUILoading, openSnackbar,
+  SetUILoadingAction, SetUIStopLoadingAction, OpenSnackbarAction,
+} from 'src/Store/ui/UIActions';
+import {
+  saveUserData, SaveUserAction, logoutUser, LogoutUserAction,
+} from 'src/Store/user/UserActions';
+import firebase from './firebase';
+
+export const signInWithEmailPassword = (email: string, password: string) => (
+  dispatch: (
+    arg0: SetUILoadingAction | SetUIStopLoadingAction | SaveUserAction | OpenSnackbarAction
+  ) => void,
+): void => {
+  dispatch(setUILoading());
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      dispatch(saveUserData(result.user));
+      dispatch(openSnackbar('You have been successfully signed in.'));
+      dispatch(clearUILoading());
+    })
+    .catch((error) => {
+      dispatch(openSnackbar(error.message));
+      dispatch(clearUILoading());
+    });
+};
+
+export const signOut = () => (
+  dispatch: (arg0: OpenSnackbarAction | LogoutUserAction) => void,
+): void => {
+  firebase.auth().signOut()
+    .then(() => {
+      dispatch(openSnackbar('You have been successfully signed out.'));
+      dispatch(logoutUser());
+    })
+    .catch((error) => dispatch(openSnackbar(error.message)));
+};
