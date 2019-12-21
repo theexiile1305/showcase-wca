34ecb67ff279ae1338340b8f31058b2839a3a160
theexiile1305 <michael.fuchs@hm.edu>
2019-12-21 16:28:33 +0100

    basic redux setup

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,12 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import reducers from './Reducers';
+import RootReducer from './RootReducer';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 const initialState = {};
 const middleware = [thunk];
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
-const store = createStore(reducers, initialState, enhancer);
+const store = createStore(RootReducer, initialState, enhancer);
 
 export default store;
