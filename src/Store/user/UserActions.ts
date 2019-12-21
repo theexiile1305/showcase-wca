2ec0344ecb849b4d8058cec7bced7d6334acd1e4
theexiile1305 <michael.fuchs@hm.edu>
2019-12-21 16:28:58 +0100

    redux: user components

---
+++ src/Store/user/UserActions.ts
@@ -0,0 +1,33 @@
+import { Action, ActionCreator } from 'redux';
+import { MUser } from 'src/Models/MUser';
+
+export enum UserAction {
+  SAVE_USER = '@@wca/SAVE_USER',
+  LOGOUT_USER = '@@wca/LOGOUT_USER'
+}
+
+export interface SaveUserAction extends Action {
+  type: UserAction.SAVE_USER;
+  user: MUser;
+}
+
+export interface LogoutUserAction extends Action {
+  type: UserAction.LOGOUT_USER;
+}
+
+export type UserActions =
+  | SaveUserAction
+  | LogoutUserAction;
+
+export const saveUserData: ActionCreator<SaveUserAction> = (user: firebase.User) => ({
+  type: UserAction.SAVE_USER,
+  user: {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+  },
+});
+
+export const logoutUser: ActionCreator<LogoutUserAction> = () => ({
+  type: UserAction.LOGOUT_USER,
+});
