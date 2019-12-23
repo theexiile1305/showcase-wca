6b14b48b85c10bac06eba47f64e7b317523d4d07
theexiile1305 <michael.fuchs@hm.edu>
2019-12-23 20:44:06 +0100

    first draft of welcome screen

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,22 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 import {
-  Container, createStyles, makeStyles, Theme,
+  Grid, Paper, Typography, Button,
 } from '@material-ui/core';
+import {
+  IDENTITIES, DOCUMENTS, DEBUG, USER,
+} from 'src/Routes';
+import style from 'src/Styles';
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-}));
 
 const Home: React.FC = () => {
-  const classes = useStyles();
+  const classes = style();
+
+  const renderLink = (link: string): React.ReactFragment => (
+    <Button
+      color="inherit"
+      component={Link}
+      to={link}
+      startIcon={<FontAwesomeIcon icon={faLink} />}
+    />
+  );
+
+  const renderPaper = (
+    size: 12 | 6, title: string, text: string, link?: string,
+  ): React.ReactFragment => (
+    <Grid item xs={size}>
+      <Paper className={classes.paper}>
+        <Typography variant="h5" component="h3">
+          {title}
+          {link ? renderLink(link) : (<></>)}
+        </Typography>
+        <Typography component="p">{text}</Typography>
+      </Paper>
+    </Grid>
+  );
 
   return (
-    <Container component="main" className={classes.main} maxWidth="sm">
-      <div>Home</div>
-    </Container>
+    <Grid container spacing={3}>
+      {renderPaper(
+        12,
+        'Welcome to the Shwocase of the Web Cryptography API',
+        'In the course of my bachelor thesis \'Evaluation and Implementation of a secure cloud storage with the Web Cryptography API\' this application was implemented to show some basic cryptographic operations in web applications. This case study is a proof-of-concept to securely store, exchange and sign documents in the public cloud. Furthermore, all entities of the user- and identities management are encrypted on a client-side basis, enabling full end-to-end encryption.',
+      )}
+      {renderPaper(
+        6,
+        'User Management',
+        'To use this application, you must register. A wide variety of settings can be made in user management. Just try it out for yourself!',
+        USER,
+      )}
+      {renderPaper(
+        6,
+        'Identities Management',
+        'In this section, all identities required for secure message exchange can be managed.',
+        IDENTITIES,
+      )}
+      {renderPaper(
+        12,
+        'Secure Cloud Storage & Document Exchange',
+        'All stored files are displayed in this table so that they can be downloaded or deleted. Of course, new files can also be uploaded or exchanged with a contact person. Nevertheless, the data exchange requires the public key of the communication partner. ',
+        DOCUMENTS,
+      )}
+      {renderPaper(
+        12,
+        'Debug Information',
+        'The debug section can be used to inspect cryptography keys and perform different cryptographic operations manually. Try it Out!',
+        DEBUG,
+      )}
+    </Grid>
   );
 };
 
