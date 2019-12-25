16af76cfd9683764475a77d5c4f0803d75964063
Michael Fuchs <michael.fuchs@hm.edu>
2019-12-25 02:51:29 +0100

    Authenticated Routes #25 (#27)

--- src/Routes/AuthRoute.tsx
+++ src/Routes/AuthRoute.tsx
@@ -6,6 +6,7 @@ import { SIGN_IN } from '.';
 
 export type AuthRouteProps = RouteProps
 
+// eslint-disable-next-line react/prop-types
 const AuthRoute: React.FC<AuthRouteProps> = ({ path, component }) => {
   const isAuthenticated = useSelector((state: ApplicationState) => state.user.isAuthenticated);
 
